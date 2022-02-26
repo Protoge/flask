4eb1b787871d65a7b5512bf853502f5715f2911a
rj <rjgilo2799@gmail.com>
2022-02-25 22:40:01 -0500

    simple pages

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -15,7 +15,6 @@ def test_request_main_menu_links(client):
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
-    assert response.status_code == 200
     assert b"Index Page" in response.data
 
 
@@ -23,7 +22,7 @@ def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
-
+    assert b"About Page" in response.data
 
 
 def test_request_page1(client):
