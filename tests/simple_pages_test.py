9d71606e44732210da2e70fd839afa3890a39200
rj <rjgilo2799@gmail.com>
2022-02-25 22:26:41 -0500

    simple pages

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -15,7 +15,7 @@ def test_request_main_menu_links(client):
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
-    assert response.status_code != 200
+    assert response.status_code == 200
     assert b"Index Page" in response.data
 
 
